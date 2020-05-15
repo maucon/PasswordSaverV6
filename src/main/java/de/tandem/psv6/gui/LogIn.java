a00fdc151345b08b821e4d86c7e03c8f77ec92b8
TheSwerik <derswerik@gmail.com>
2020-05-15 14:55:26 +0200

    add Name and Password

--- src/main/java/de/tandem/psv6/gui/LogIn.java
+++ src/main/java/de/tandem/psv6/gui/LogIn.java
@@ -6,8 +6,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -38,6 +41,7 @@ public class LogIn {
     }
 
     private void initNodes() {
+        // Buttons:
         var loginButton = new Button("Login");
         loginButton.resize(200 * app.getHScale(), 100 * app.getVScale());
         var exitButton = new Button("Exit");
@@ -47,12 +51,24 @@ public class LogIn {
         hBox.setAlignment(Pos.CENTER);
         HBox.setMargin(loginButton, new Insets(50, 100, 50, 50));
         HBox.setMargin(exitButton, new Insets(50, 50, 50, 100));
-
         root.setBottom(hBox);
+
         loginButton.setOnAction(event -> {
             stage.close();
             app.getStage().show();
         });
         exitButton.setOnAction(event -> stage.close());
+
+        // Input Fields:
+        var nameLabel = new Label("Name:");
+        var nameInput = new TextField();
+        var nameHBox = new HBox(nameLabel, nameInput);
+
+        var passwordLabel = new Label("Password:");
+        var passwordInput = new TextField();
+        var passwordHBox = new HBox(passwordLabel, passwordInput);
+
+        var formVBox = new VBox(nameHBox, passwordHBox);
+        root.setCenter(formVBox);
     }
 }
