8814840a1f0a352a405dba3ae899dce743918f30
TheSwerik <derswerik@gmail.com>
2020-05-15 15:48:58 +0200

    add Register Dialog

--- src/main/java/de/tandem/psv6/gui/LogIn.java
+++ src/main/java/de/tandem/psv6/gui/LogIn.java
@@ -1,10 +1,10 @@
 package de.tandem.psv6.gui;
 
 import de.tandem.psv6.App;
+import de.tandem.psv6.gui.dialogs.RegisterDialog;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -55,20 +55,22 @@ public class LogIn {
         // Buttons:
         var loginButton = new Button("Login");
         loginButton.resize(200 * getHScale(), 100 * getVScale());
-        var exitButton = new Button("Exit");
-        exitButton.resize(200 * getHScale(), 100 * getVScale());
+        var registerButton = new Button("Register");
+        registerButton.resize(200 * getHScale(), 100 * getVScale());
 
-        var hBox = new HBox(loginButton, exitButton);
+        var hBox = new HBox(registerButton, loginButton);
         hBox.setAlignment(Pos.CENTER);
-        HBox.setMargin(loginButton, new Insets(50 * getVScale(), 100 * getHScale(), 50 * getVScale(), 50 * getHScale()));
-        HBox.setMargin(exitButton, new Insets(50 * getVScale(), 50 * getHScale(), 50 * getVScale(), 100 * getHScale()));
+        hBox.setSpacing(80 * getHScale());
+        hBox.setMinHeight(60 * getVScale());
+//        HBox.setMargin(loginButton, new Insets(50 * getVScale(), 100 * getHScale(), 50 * getVScale(), 50 * getHScale()));
+//        HBox.setMargin(registerButton, new Insets(50 * getVScale(), 50 * getHScale(), 50 * getVScale(), 100 * getHScale()));
         root.setBottom(hBox);
 
         loginButton.setOnAction(event -> {
             stage.close();
             app.getStage().show();
         });
-        exitButton.setOnAction(event -> stage.close());
+        registerButton.setOnAction(event -> new RegisterDialog(app, stage, 200 * getHScale(), 250 * getVScale()));
 
         // Input Fields:
         var grid = new GridPane();
