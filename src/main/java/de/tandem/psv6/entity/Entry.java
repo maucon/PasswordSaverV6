a26a43ca3340f496b656dc284adadc3c7f31f0e2
TheSwerik <derswerik@gmail.com>
2020-05-15 13:16:31 +0200

    cleanup

--- src/main/java/de/tandem/psv6/entity/Entry.java
+++ src/main/java/de/tandem/psv6/entity/Entry.java
@@ -1,13 +1,11 @@
 package de.tandem.psv6.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class Entry implements Serializable {
 
