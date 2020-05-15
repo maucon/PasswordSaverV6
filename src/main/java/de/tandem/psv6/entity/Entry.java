071800dad502cadc186f6b97eb5aa35f091fcee2
maucon <maucon13@gmail.com>
2020-05-15 13:20:40 +0200

    edited Entry

--- src/main/java/de/tandem/psv6/entity/Entry.java
+++ src/main/java/de/tandem/psv6/entity/Entry.java
@@ -2,13 +2,16 @@ package de.tandem.psv6.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.identityconnectors.common.security.GuardedString;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 public class Entry implements Serializable {
-    private String name;
-    private String login;
-    private String password;
+
+    private GuardedString name;
+    private GuardedString login;
+    private GuardedString password;
+
 }
