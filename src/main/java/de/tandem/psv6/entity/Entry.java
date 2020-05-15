d7e2bc3ec5a1ea4b4079c96ce35a0ba81fa496f9
TheSwerik <derswerik@gmail.com>
2020-05-15 13:18:35 +0200

    pls maucon pls

--- src/main/java/de/tandem/psv6/entity/Entry.java
+++ src/main/java/de/tandem/psv6/entity/Entry.java
@@ -8,9 +8,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 public class Entry implements Serializable {
-
     private String name;
     private String login;
     private String password;
-
 }
