34d9bbb4138bfb216ac5c30637a8b7b2407fba57
maucon <maucon13@gmail.com>
2020-05-15 15:59:14 +0200

    changed Security

--- src/main/java/de/tandem/psv6/security/Security.java
+++ src/main/java/de/tandem/psv6/security/Security.java
@@ -3,11 +3,14 @@ package de.tandem.psv6.security;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
-import java.util.Base64;
 
 public class Security {
 
@@ -21,44 +24,18 @@ public class Security {
         return DigestUtils.sha512_256Hex(string);
     }
 
-    public static String encrypt(String text, String key) throws GeneralSecurityException {
-        return bytesToHex(Base64.getEncoder().encode(initCipher(Cipher.ENCRYPT_MODE, key).doFinal(text.getBytes(StandardCharsets.UTF_8))));
+    public static CipherOutputStream encryptStream(FileOutputStream fileOutputStream, String key) throws GeneralSecurityException {
+        return new CipherOutputStream(fileOutputStream, initCipher(Cipher.ENCRYPT_MODE, key));
     }
 
-    public static String decrypt(String text, String key) throws GeneralSecurityException {
-        return new String(initCipher(Cipher.DECRYPT_MODE, key).doFinal(Base64.getDecoder().decode(hexToBytes(text))));
+    public static CipherInputStream decryptStream(FileInputStream fileInputStream, String key) throws GeneralSecurityException {
+        return new CipherInputStream(fileInputStream, initCipher(Cipher.DECRYPT_MODE, key));
     }
 
     private static Cipher initCipher(int mode, String key) throws GeneralSecurityException {
-        var secretKey = new SecretKeySpec(Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8), 32), "AES");
-
         var cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(mode, secretKey);
-
+        cipher.init(mode, new SecretKeySpec(Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8), 32), "AES"));
         return cipher;
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        var hexChars = new char[bytes.length * 2];
-
-        for (int j = 0; j < bytes.length; j++) {
-            var v = bytes[j] & 0xFF;
-
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-
-        return new String(hexChars);
-    }
-
-    private static byte[] hexToBytes(String hexChars) {
-        var len = hexChars.length();
-        var data = new byte[len / 2];
-
-        for (int i = 0; i < len; i += 2)
-            data[i / 2] = (byte) ((Character.digit(hexChars.charAt(i), 16) << 4) + Character.digit(hexChars.charAt(i + 1), 16));
-
-        return data;
     }
 
 }
