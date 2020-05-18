9d783626d8d9ef041297cd84df5b0e1dbd8344d7
TheSwerik <derswerik@gmail.com>
2020-05-18 16:33:36 +0200

    login & register works

--- src/main/java/de/tandem/psv6/gui/dialogs/RegisterDialog.java
+++ src/main/java/de/tandem/psv6/gui/dialogs/RegisterDialog.java
@@ -1,11 +1,17 @@
 package de.tandem.psv6.gui.dialogs;
 
+import de.tandem.psv6.database.Database;
+import de.tandem.psv6.entity.User;
 import de.tandem.psv6.gui.GUIOwner;
+import de.tandem.psv6.security.Security;
+import javafx.scene.control.ComboBox;
 
 public class RegisterDialog extends Dialog {
+    private final ComboBox<String> comboBox;
 
-    public RegisterDialog(GUIOwner owner) {
+    public RegisterDialog(GUIOwner owner, ComboBox<String> comboBox) {
         super(owner, "Register", 500, 400);
+        this.comboBox = comboBox;
     }
 
     @Override
@@ -27,7 +33,9 @@ public class RegisterDialog extends Dialog {
             else if (password.isBlank()) new ErrorDialog(owner, stage, "Password is blank.");
             else if (!password.equals(password2)) new ErrorDialog(owner, stage, "Passwords don't match.");
             else {
-                // TODO save User
+                Database.setupUserFolder(new User(name, Security.save_hash(name, password)));
+                comboBox.getItems().clear();
+                comboBox.getItems().addAll(Database.getUserList());
                 stage.close();
             }
         });
