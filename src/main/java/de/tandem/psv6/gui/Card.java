b38671de542f1bd0e65092a8fed4dcf2a032c9ce
TheSwerik <derswerik@gmail.com>
2020-05-19 15:36:59 +0200

    add CreateDialog

--- src/main/java/de/tandem/psv6/gui/Card.java
+++ src/main/java/de/tandem/psv6/gui/Card.java
@@ -2,6 +2,7 @@ package de.tandem.psv6.gui;
 
 import de.tandem.psv6.App;
 import de.tandem.psv6.entity.Entry;
+import de.tandem.psv6.gui.dialogs.CreateEntryDialog;
 import de.tandem.psv6.gui.dialogs.EntryDialog;
 import de.tandem.psv6.gui.dialogs.RemoveConfirmDialog;
 import javafx.scene.control.Label;
@@ -20,6 +21,17 @@ public class Card {
             if (e.getButton() == MouseButton.SECONDARY) new RemoveConfirmDialog(app, entry);
         });
 
+        pane.setPrefWidth(200 * app.getHScale());
+        pane.setPrefHeight(200 * app.getVScale());
+        return pane;
+    }
+
+    public static BorderPane addEntryCard(App app) {
+        var pane = new BorderPane();
+        pane.setCenter(new Label("+"));
+        pane.setId("Card");
+        pane.setStyle(app.getStyle());
+        pane.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> new CreateEntryDialog(app));
         pane.setPrefWidth(200 * app.getHScale());
         pane.setPrefHeight(200 * app.getVScale());
         return pane;
