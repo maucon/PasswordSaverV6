b469e3dabbed5f0a335b7e526876c19a24191951
TheSwerik <derswerik@gmail.com>
2020-05-19 15:23:08 +0200

    add Remove

---
+++ src/main/java/de/tandem/psv6/gui/dialogs/RemoveConfirmDialog.java
@@ -0,0 +1,18 @@
+package de.tandem.psv6.gui.dialogs;
+
+import de.tandem.psv6.App;
+import de.tandem.psv6.database.Database;
+import de.tandem.psv6.entity.Entry;
+import javafx.scene.control.Label;
+
+public class RemoveConfirmDialog extends Dialog {
+    public RemoveConfirmDialog(App app, Entry entry) {
+        super(app, "Remove?", 400, 200);
+        grid.addRow(0, new Label("Are you sure you want to remove " + entry.getName() + " ?"));
+        addOkCancelButtons(1, true, 100).onActionProperty().set(event -> {
+            app.removeEntry(entry);
+            Database.getInstance().removeEntry(entry);
+            stage.close();
+        });
+    }
+}
