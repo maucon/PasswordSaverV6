384c0bf255ccaa642f56297be2c3250b88ce89d2
TheSwerik <derswerik@gmail.com>
2020-05-19 14:55:25 +0200

    cleanup

--- src/main/java/de/tandem/psv6/gui/Card.java
+++ src/main/java/de/tandem/psv6/gui/Card.java
@@ -3,7 +3,6 @@ package de.tandem.psv6.gui;
 import de.tandem.psv6.App;
 import de.tandem.psv6.entity.Entry;
 import de.tandem.psv6.gui.dialogs.EntryDialog;
-import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -11,20 +10,12 @@ import javafx.scene.layout.BorderPane;
 public class Card {
     public static BorderPane createCard(App app, Entry entry) {
         var pane = new BorderPane();
-
         pane.setCenter(new Label(entry.getName()));
         pane.setId("Card");
         pane.setStyle(app.getStyle());
-        pane.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println(entry.toString());
-                new EntryDialog(app, entry);
-            }
-        });
+        pane.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> new EntryDialog(app, entry));
         pane.setPrefWidth(200 * app.getHScale());
         pane.setPrefHeight(200 * app.getVScale());
-
         return pane;
     }
 }
