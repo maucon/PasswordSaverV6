3fc20c2f9bbab79dc05b6cf08e14522d762f6d96
TheSwerik <derswerik@gmail.com>
2020-05-21 19:45:34 +0200

    add Enter for closing Error DIalog

--- src/main/java/de/tandem/psv6/gui/dialogs/ErrorDialog.java
+++ src/main/java/de/tandem/psv6/gui/dialogs/ErrorDialog.java
@@ -1,7 +1,10 @@
 package de.tandem.psv6.gui.dialogs;
 
 import de.tandem.psv6.gui.GUIOwner;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class ErrorDialog extends Dialog {
@@ -13,6 +16,12 @@ public class ErrorDialog extends Dialog {
     public ErrorDialog(GUIOwner owner, Stage ownerStage, String errorMessage) {
         super(owner, ownerStage, "Error", errorMessage.length() * 13, 200);
         grid.addRow(0, new Label(errorMessage));
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode().equals(KeyCode.ENTER)) stage.close();
+            }
+        });
     }
 
 }
