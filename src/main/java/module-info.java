060355246f8f52288549bd07c0e107f130eb5286
TheSwerik <derswerik@gmail.com>
2020-05-26 14:58:59 +0200

    fix database folder not created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module PasswordSaver {
     requires static lombok;
-//    requires javafx.base;
     requires javafx.controls;
     requires connector.framework;
     requires org.apache.commons.codec;
+    requires java.desktop;
 
     exports de.tandem.psv6;
 }
